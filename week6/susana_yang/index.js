a0f9e7b781d5247d39536940b216ce099b228759
sevenminusone <susanayang@berkeley.edu>
2025-03-13 23:52:59 -0700

    week 6

--- week6/susana_yang/index.js
+++ week6/susana_yang/index.js
@@ -5,19 +5,37 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let users = {};
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  users[socket.id] = "Anonymous";
+  io.emit('update users', Object.values(users));
+
+  socket.on('set nickname', (nickname) => {
+    users[socket.id] = nickname;
+    io.emit('update users', Object.values(users)); 
+  });
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log(`${users[socket.id]} disconnected`);
+    delete users[socket.id]; // Remove user
+    io.emit('update users', Object.values(users)); // Update user list
   });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', users[socket.id]); // Notify others
+  });
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    io.emit('chat message', `${users[socket.id]}: ${msg}`);
+    console.log(`${users[socket.id]}: ${msg}`);
   });
+
 });
 
 server.listen(3000, () => {
